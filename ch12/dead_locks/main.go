2a330121fd72a108528a0d1772362e75404dea17
Jørgen Andersen <jorg1piano@gmail.com>
2024-03-29 03:34:22 +0100

    Add a couple of comments to better understand how the deadlock happend

--- ch12/dead_locks/main.go
+++ ch12/dead_locks/main.go
@@ -6,15 +6,21 @@ func main() {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 
+	// This cannot proceed until ch1 is read and the main goroutine cannot proceed until ch2 is read
 	go func() {
 		inGorutine := 1
+		// Write inGorutine to ch1
 		ch1 <- inGorutine
+		// read variable from ch2 into fromMain
 		fromMain := <-ch2
 		fmt.Println("goroutine:", inGorutine, fromMain)
 	}()
 
 	inMain := 2
+	// write inMain to ch2
 	ch2 <- inMain
+
+	// We never get here (read from ch1)
 	fromGoroutine := <-ch1
 	fmt.Println("main:", inMain, fromGoroutine)
 }
