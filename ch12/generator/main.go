fac73345a727902933dee02ddb106d495c581a4e
Jørgen Andersen <jorg1piano@gmail.com>
2024-03-29 03:59:50 +0100

    Use context to cancel channel

--- ch12/generator/main.go
+++ ch12/generator/main.go
@@ -1,23 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
 
 // Just an example, dont use go routines and channels like this
 func main() {
-	for i := range countTo(10) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	ch := countTo(ctx, 10)
+	for i := range ch {
+		if i > 4 {
+			break
+		}
 		fmt.Println(i)
 	}
+
+	cancel()
 }
 
 // <-chan int means "return a recieve only channel that produces int's"
-func countTo(max int) <-chan int {
+func countTo(ctx context.Context, max int) <-chan int {
 	ch := make(chan int)
 
 	go func() {
+		defer close(ch)
+
 		for i := 0; i < max; i++ {
-			ch <- i
+			select {
+			case <-ctx.Done():
+				fmt.Println("Done hit")
+				return
+			case ch <- i:
+			}
 		}
-		close(ch)
 	}()
 	return ch
 }
